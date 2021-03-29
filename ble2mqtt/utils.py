d01a9d19486deee220d87dca5dabbd4baaed4614
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-03-29 10:47:02 +0300

    Percentage cannot be negative

--- ble2mqtt/utils.py
+++ ble2mqtt/utils.py
@@ -7,7 +7,7 @@ def format_binary(data: bytes):
 
 
 def cr2032_voltage_to_percent(mvolts: int):
-    return min(int(round((mvolts/1000 - 2.1), 2) * 100), 100)
+    return max(min(int(round((mvolts/1000 - 2.1), 2) * 100), 100), 0)
 
 
 def rssi_to_linkquality(rssi):
