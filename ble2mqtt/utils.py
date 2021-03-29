1d70b8ed4db89934a06185d53381e14ba44e1d0a
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-03-29 11:19:59 +0300

    Adjust percentage calculation from voltage

--- ble2mqtt/utils.py
+++ ble2mqtt/utils.py
@@ -7,7 +7,8 @@ def format_binary(data: bytes, delimiter=' '):
 
 
 def cr2032_voltage_to_percent(mvolts: int):
-    return max(min(int(round((mvolts/1000 - 2.1), 2) * 100), 100), 0)
+    coeff = 0.8  # >2.9V counts as 100% = (2900 - 2100)/100
+    return max(min(int(round((mvolts/1000 - 2.1)/coeff, 2) * 100), 100), 0)
 
 
 def rssi_to_linkquality(rssi):
