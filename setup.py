d9fc07d0404f6dd1d5adb0f3e7ea36e10d17ca81
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-11-15 12:29:37 +0300

    Update classifiers

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ setup(
         'bleak>=0.12.0',
     ],
     classifiers=[
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Utilities',
     ],
