bffcf5e6a0d5488f06981096d883ce83176c8ece
j4cbo <jacob@j4cbo.com>
2023-12-20 01:52:15 -0500

    Add Govee H5074 temperature/humidity sensor support (#77)
    * Add Govee H5074 temperature/humidity sensor support
    
    * Fix humidity parsing
    
    * Clean up comments
    
    * Code review fixes
    
    ---------
    
    Co-authored-by: Jacob Potter <jacobdp@gmail.com>

---
+++ ble2mqtt/protocols/govee.py
@@ -0,0 +1,31 @@
+"""
+Decoder for Govee temperature/humidity sensors.
+Based on https://github.com/wcbonner/GoveeBTTempLogger/blob/master/goveebttemplogger.cpp (MIT License)
+"""
+from __future__ import annotations
+
+import struct
+
+
+class GoveeDecoder:
+    def __init__(self, raw_data: bytes) -> None:
+        # note: currently only H5074 is supported
+        if len(raw_data) != 7:
+            raise ValueError("Govee data must be 7 bytes long")
+        self.data: tuple[int, ...] = struct.unpack("<xhhBx", raw_data)
+
+    @property
+    def temperature_celsius(self) -> float | None:
+        if self.data[0] == -32768:
+            return None
+        return round(self.data[0] / 100.0, 2)
+
+    @property
+    def humidity_percentage(self) -> float | None:
+        if self.data[0] == -32768:
+            return None
+        return round(self.data[1] / 100.0, 2)
+
+    @property
+    def battery_percentage(self) -> int | None:
+        return self.data[2]
