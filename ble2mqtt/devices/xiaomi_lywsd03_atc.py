40dcef062c4cd4d938cd41fc21675b00d9a22213
Quintin Hill <stuff@quintin.me.uk>
2024-01-12 21:59:19 +0000

    xiaomi_lywsd03_atc: make send_custom a class variable
    This makes the data class more generic.

--- ble2mqtt/devices/xiaomi_lywsd03_atc.py
+++ ble2mqtt/devices/xiaomi_lywsd03_atc.py
@@ -15,7 +15,6 @@ class SensorState:
     battery: int = 0
     temperature: float = 0
     humidity: float = 0
-    sends_custom: bool = False  # support decimal value for humidity
 
 
 class XiaomiHumidityTemperatureLYWSDATC(XiaomiHumidityTemperature):
@@ -24,15 +23,18 @@ class XiaomiHumidityTemperatureLYWSDATC(XiaomiHumidityTemperature):
     SUPPORT_PASSIVE = True
     SUPPORT_ACTIVE = False
 
+    def __init__(self, mac, *args, **kwargs) -> None:
+        super().__init__(mac, *args, **kwargs)
+        self._sends_custom = False  # support decimal value for humidity
+
     def handle_advert(self, scanned_device: BLEDevice, adv_data):
         service_data = adv_data.service_data
         adv_data = service_data.get(str(ENVIRONMENTAL_SENSING))
 
         if adv_data:
-            sends_custom = bool(self._state and self._state.sends_custom)
             if len(adv_data) == 15:
                 # b'\xe6o\xb98\xc1\xa4\x95\t\xff\x08~\x0cd\xe0\x04'
-                sends_custom = True
+                self._sends_custom = True
                 adv_data = bytes(adv_data)
                 self._state = self.SENSOR_CLASS(
                     temperature=int.from_bytes(
@@ -40,11 +42,10 @@ class XiaomiHumidityTemperatureLYWSDATC(XiaomiHumidityTemperature):
                     humidity=int.from_bytes(
                         adv_data[8:10], byteorder='little') / 100,
                     battery=adv_data[12],
-                    sends_custom=sends_custom,
                 )
 
             elif len(adv_data) == 13:
-                if sends_custom:
+                if self._sends_custom:
                     # ignore low res humidity packets
                     return
                 # [a4 c1 38 84 7e 97 01 26 15 50 0b 73 17]
@@ -55,7 +56,6 @@ class XiaomiHumidityTemperatureLYWSDATC(XiaomiHumidityTemperature):
                         adv_data[6:8], byteorder='big', signed=True) / 10,
                     humidity=adv_data[8],
                     battery=adv_data[9],
-                    sends_custom=sends_custom,
                 )
                 _LOGGER.debug(
                     f'Advert received for {self}, {format_binary(adv_data)}, '
