33322ab8a22ec19d58633f9c4e751fdcbd1aca97
Ivan Belokobylskiy <belokobylskij@gmail.com>
2024-09-13 10:28:55 +0300

    oregon: more chars

--- ble2mqtt/devices/oregon_ht.py
+++ ble2mqtt/devices/oregon_ht.py
@@ -1,106 +1,159 @@
 import logging
 import struct
-import uuid
 import typing as ty
-from dataclasses import dataclass
+import uuid
+from collections import defaultdict
+from dataclasses import dataclass, field
 
-from .base import (
-    SENSOR_DOMAIN,
-    ConnectionMode,
-    Sensor,
-    SubscribeAndSetDataMixin
-)
-from .uuids import BATTERY, MANUFACTURER, MODEL_NAME, FIRMWARE_VERSION
+from .base import (SENSOR_DOMAIN, ConnectionMode, Sensor,
+                   SubscribeAndSetDataMixin)
+from .uuids import BATTERY, FIRMWARE_VERSION, MANUFACTURER, MODEL_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
-
+DEVICE_INFORMATION_UUID = uuid.UUID("74e78e02-c6a4-11e2-b7a9-0002a5d5c51b")
 INDOOR_AND_CH1_TO_3_TH_DATA_UUID = uuid.UUID(
     '74e78e10-c6a4-11e2-b7a9-0002a5d5c51b',
 )
+CH4_TO_7_TH_DATA = uuid.UUID('74e78e14-c6a4-11e2-b7a9-0002a5d5c51b')
+PRESSURE_DATA = uuid.UUID('74e78e20-c6a4-11e2-b7a9-0002a5d5c51b')
+
 FIRST_PACKET = 0x01
 SECOND_PACKET = 0x82
+SENDER_0_3 = 0x17
+SENDER_4_7 = 0x20
+SENDER_8_11 = 0x1a
+
+NONE_BYTE = 0x7f
+NONE_WORD = 0x7fff
+
+
+@dataclass
+class Temperature:
+    value: ty.Optional[float] = None
+    min: ty.Optional[float] = None
+    max: ty.Optional[float] = None
+
+    def is_valid(self):
+        return any(
+            getattr(self, attr) is not None
+            for attr in self.__annotations__
+        )
+
+
+@dataclass
+class Humidity:
+    value: ty.Optional[int] = None
+    min: ty.Optional[int] = None
+    max: ty.Optional[int] = None
+
+    def is_valid(self):
+        return any(
+            getattr(self, attr) is not None
+            for attr in self.__annotations__
+        )
 
 
 @dataclass
 class SensorState:
-    indoor_temperature: ty.Optional[float]
-    indoor_temp_min: ty.Optional[float]
-    indoor_temp_max: ty.Optional[float]
-    outdoor_temperature: ty.Optional[float]
-    outdoor_temp_min: ty.Optional[float]
-    outdoor_temp_max: ty.Optional[float]
-    humidity: ty.Optional[int]
-    humidity_min: ty.Optional[int]
-    humidity_max: ty.Optional[int]
+    temp_trend: ty.Optional[float] = None
+    humid_trend: ty.Optional[int] = None
+    temperatures: ty.List[Temperature] = field(
+        default_factory=lambda: [Temperature() for _ in range(4)])
+    humidities: ty.List[Humidity] = field(
+        default_factory=lambda: [Humidity() for _ in range(4)])
+    pressure: ty.Optional[int] = None
     battery: int = 0
 
-    @classmethod
-    def extract_word(cls, value: int) -> float:
-        return round(value / 10, 2) if value != 0x7FFF else None
+    def is_ready(self):
+        return any(
+            getattr(self, attr) is not None
+            for attr in self.__annotations__
+            if attr != 'battery'
+        )
 
     @classmethod
-    def from_data(cls, data1: bytes, data2: bytes, battery: int):
+    def extract_byte(cls, value: int) -> ty.Optional[int]:
+        return value if value != NONE_BYTE else None
+
+    @classmethod
+    def extract_word(cls, value: int) -> ty.Optional[float]:
+        return round(value / 10, 2) if value != NONE_WORD else None
+
+    def populate_0_3(self, data1: bytes, data2: bytes):
         (
             temp0, temp1, temp2, temp3, humid0, humid1, humid2, humid3,
             temp_trend, humid_trend, humid0_max, humid0_min, humid1_max,
             humid1_min, humid2_max,
-         ) = struct.unpack('<x4h11b', data1)
+        ) = struct.unpack('<x4h11b', data1)
+        self.temperatures[0].value = self.extract_word(temp0)
+        self.temperatures[1].value = self.extract_word(temp1)
+        self.temperatures[2].value = self.extract_word(temp2)
+        self.temperatures[3].value = self.extract_word(temp3)
+        self.humidities[0].value = self.extract_byte(humid0)
+        self.humidities[1].value = self.extract_byte(humid1)
+        self.humidities[2].value = self.extract_byte(humid2)
+        self.humidities[3].value = self.extract_byte(humid3)
+        self.temp_trend = self.extract_byte(temp_trend)
+        self.humid_trend = self.extract_byte(humid_trend)
+        self.humidities[0].max = self.extract_byte(humid0_max)
+        self.humidities[0].min = self.extract_byte(humid0_min)
+        self.humidities[1].max = self.extract_byte(humid1_max)
+        self.humidities[1].min = self.extract_byte(humid1_min)
+        self.humidities[2].max = self.extract_byte(humid2_max)
         (
             humid2_min, humid3_max, humid3_min, temp0_max, temp0_min,
-            temp1_max, temp1_min, temp2_max, temp2_min, temp3_max, temp3_min,
-        ) = struct.unpack('<x3B8H', data2)
-        return cls(
-            indoor_temperature=cls.extract_word(temp0),
-            indoor_temp_min=cls.extract_word(temp0_min),
-            indoor_temp_max=cls.extract_word(temp0_max),
-            outdoor_temperature=cls.extract_word(temp1),
-            outdoor_temp_min=cls.extract_word(temp1_min),
-            outdoor_temp_max=cls.extract_word(temp1_max),
-            humidity=humid0 if humid0 != 0x7f else None,
-            humidity_min=humid0_min if humid0_min != 0x7f else None,
-            humidity_max=humid0_max if humid0_max != 0x7f else None,
-            battery=battery,
-        )
+            temp1_max, temp1_min, temp2_max, temp2_min, temp3_max,
+            temp3_min,
+        ) = struct.unpack('<x3B8h', data2)
+        self.humidities[2].min = self.extract_byte(humid2_min)
+        self.humidities[3].max = self.extract_byte(humid3_max)
+        self.humidities[3].min = self.extract_byte(humid3_min)
+        self.temperatures[0].max = self.extract_word(temp0_max)
+        self.temperatures[0].min = self.extract_word(temp0_min)
+        self.temperatures[1].max = self.extract_word(temp1_max)
+        self.temperatures[1].min = self.extract_word(temp1_min)
+        self.temperatures[2].max = self.extract_word(temp2_max)
+        self.temperatures[2].min = self.extract_word(temp2_min)
+        self.temperatures[3].max = self.extract_word(temp3_max)
+        self.temperatures[3].min = self.extract_word(temp3_min)
 
 
 class OregonScientificWeatherStation(SubscribeAndSetDataMixin, Sensor):
     NAME = 'oregon_weather'
     MANUFACTURER = 'Oregon'
-    DATA_CHAR = INDOOR_AND_CH1_TO_3_TH_DATA_UUID
+    INDICATION_CHARS = [
+        INDOOR_AND_CH1_TO_3_TH_DATA_UUID,
+        # CH4_TO_7_TH_DATA,
+        # PRESSURE_DATA,
+    ]
     BATTERY_CHAR = BATTERY
     ACTIVE_CONNECTION_MODE = ConnectionMode.ACTIVE_POLL_WITH_DISCONNECT
     DEFAULT_RECONNECTION_SLEEP_INTERVAL = 300
+    TEMPERATURE_CONFIG = {
+        'name': 'temperature',
+        'device_class': 'temperature',
+        'unit_of_measurement': '\u00b0C',
+    }
+    HUMIDITY_CONFIG = {
+        'name': 'humidity',
+        'device_class': 'humidity',
+        'unit_of_measurement': '%',
+    }
 
     def __init__(self, mac, *args, **kwargs) -> None:
         super().__init__(mac, *args, **kwargs)
         self.RECONNECTION_SLEEP_INTERVAL = int(
             kwargs.get('interval', self.DEFAULT_RECONNECTION_SLEEP_INTERVAL)
         )
-        # cache notifications by 1st byte,
-        # we need 0x01 and 0x82 to populate state
-        self._notification_cache = {}
-        self._last_battery = b''
+        self._device_properties = set()
+        self._state = SensorState()
+        self._notification_cache = defaultdict(dict)
 
     @property
     def entities(self):
         return {
             SENSOR_DOMAIN: [
-                {
-                    'name': 'indoor_temperature',
-                    'device_class': 'temperature',
-                    'unit_of_measurement': '\u00b0C',
-                },
-                {
-                    'name': 'outdoor_temperature',
-                    'device_class': 'temperature',
-                    'unit_of_measurement': '\u00b0C',
-                },
-                {
-                    'name': 'humidity',
-                    'device_class': 'humidity',
-                    'unit_of_measurement': '%',
-                },
                 {
                     'name': 'battery',
                     'device_class': 'battery',
@@ -110,25 +163,78 @@ class OregonScientificWeatherStation(SubscribeAndSetDataMixin, Sensor):
             ],
         }
 
-    def filter_notifications(self, sender, data):
-        # sender is 0x17 or several requests it becomes
-        # /org/bluez/hci0/dev_58_2D_34_32_E0_69/service000c/char0017
-        return (
-            sender in (0x17, 0x20) or
-            isinstance(sender, str) and
-            (sender.endswith('0017') or sender.endswith('0020'))
-        ) and len(data) == 20
+    def entities_with_lqi(self):
+        sensor_entities = [
+            *self.entities.get(SENSOR_DOMAIN, []),
+            self.get_linkquality_description(),
+        ]
+        return {
+            **self.entities,
+            SENSOR_DOMAIN: [
+                *sensor_entities,
+                *[
+                    {
+                        **self.TEMPERATURE_CONFIG,
+                        'name': f'temperature_{i}',
+                    }
+                    for i, t in enumerate(self._state.temperatures)
+                    if t.is_valid()
+                ],
+                *[
+                    {
+                        **self.HUMIDITY_CONFIG,
+                        'name': f'humidity_{i}',
+                    }
+                    for i, h in enumerate(self._state.humidities)
+                    if h.is_valid()
+                ],
+                *([
+                    {
+                        'name': 'pressure',
+                        'device_class': 'pressure',
+                        'unit_of_measurement': 'kPa',
+                    }
+                ] if self._state.pressure is not None else []),
+            ],
+        }
 
-    def process_data(self, data: bytearray):
+    def filter_notifications(self, sender: int, data: bytes):
+        return sender in [SENDER_0_3, SENDER_4_7, SENDER_8_11]
+
+    def process_data(self, data: bytearray, sender: int):
         if data[0] in {FIRST_PACKET, SECOND_PACKET}:
-            self._notification_cache[data[0]] = data
-        if len(self._notification_cache) == 2:
-            self._state = self.SENSOR_CLASS.from_data(
-                self._notification_cache.pop(FIRST_PACKET),
-                self._notification_cache.pop(SECOND_PACKET),
-                int.from_bytes(self._last_battery, byteorder='little')
-                if self._last_battery else 0,
+            self._notification_cache[sender][data[0]] = data
+
+        to_drop = []
+        for sndr, packets_by_type in self._notification_cache:
+            if len(packets_by_type) == 2:
+                to_drop.append(sndr)
+                if sndr == SENDER_0_3:
+                    _LOGGER.warning(
+                        f'[{self}] Processing data from {sndr}: '
+                        f'{packets_by_type[FIRST_PACKET].hex(" ")} '
+                        f'{packets_by_type[SECOND_PACKET].hex(" ")}')
+                    self._state.populate_0_3(
+                        packets_by_type[FIRST_PACKET],
+                        packets_by_type[SECOND_PACKET],
+                    )
+                elif sndr == SENDER_4_7:
+                    pass
+                elif sndr == SENDER_8_11:
+                    pass
+        for sndr in to_drop:
+            self._notification_cache.pop(sndr)
+
+    def _get_indication_chars(self):
+        # some models don't expose pressure characteristic,
+        # we omit it for subscribing
+        return [
+            ch for ch in self.INDICATION_CHARS
+            if any(
+                ch == uuid.UUID(cch.uuid)
+                for cch in self.client.characteristics
             )
+        ]
 
     async def get_device_data(self):
         manufacturer = await self._read_with_timeout(MANUFACTURER)
@@ -140,5 +246,11 @@ class OregonScientificWeatherStation(SubscribeAndSetDataMixin, Sensor):
         version = await self._read_with_timeout(FIRMWARE_VERSION)
         if isinstance(version, (bytes, bytearray)):
             self._version = version.decode().strip('\0')
-        self._last_battery = await self._read_with_timeout(self.BATTERY_CHAR)
+        self._state.battery = (
+            await self._read_with_timeout(self.BATTERY_CHAR)
+        )[0]
+        dev_props = await self._read_with_timeout(DEVICE_INFORMATION_UUID)
+        _LOGGER.warning(f'[{self}] Device properties: {dev_props}')
+        if dev_props[1]:
+            self._device_properties.add('indoor')
         await super().get_device_data()
